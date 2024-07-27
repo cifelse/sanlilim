49959da1ec4b05284c8d15cabd6cb52caefcc067
cifelse <meetcif@gmail.com>
2024-07-27 23:32:41 +0800

    Change to accommodate scroll speed

--- src/app/components/NavLink.tsx
+++ src/app/components/NavLink.tsx
@@ -1,5 +1,4 @@
 import React from 'react';
-import Link from 'next/link';
 
 interface NavLinkProps {
     href: string;
@@ -7,18 +6,27 @@ interface NavLinkProps {
     isActive: boolean;
     isEarthquakeSection: boolean;
     mobile?: boolean;
+    onClick: (event: React.MouseEvent<HTMLAnchorElement>) => void;
 }
 
-const NavLink: React.FC<NavLinkProps> = ({ href, children, isActive, isEarthquakeSection, mobile = false }) => {
+const NavLink: React.FC<NavLinkProps> = ({ href, children, isActive, isEarthquakeSection, mobile = false, onClick }) => {
     const baseClasses = `relative group tracking-tighter ${isEarthquakeSection ? 'text-[#F05454]' : 'text-white'}`;
     const mobileClasses = 'block py-2';
     const desktopClasses = '';
 
     return (
-    <Link href={href} className={`${baseClasses} ${mobile ? mobileClasses : desktopClasses}`}>
-        {children}
-        <span className={`absolute bottom-0 left-0 w-full h-0.5 transform transition-transform duration-300 ${isActive ? 'scale-x-100' : 'scale-x-0 group-hover:scale-x-100'} ${isEarthquakeSection ? 'bg-[#F05454]' : 'bg-white'}`}></span>
-    </Link>
+        <a 
+            href={href} 
+            className={`${baseClasses} ${mobile ? mobileClasses : desktopClasses}`}
+            onClick={onClick}
+        >
+            {children}
+            <span className={`absolute bottom-0 left-0 w-full h-0.5 transform transition-transform duration-300 
+                ${isActive ? 'scale-x-100' : 'scale-x-0'} 
+                group-hover:scale-x-100
+                ${isEarthquakeSection ? 'bg-[#F05454]' : 'bg-white'}`}
+            />
+        </a>
     );
 };
 
